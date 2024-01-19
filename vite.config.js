b52dbbef14d39fadc9c08ebf35a507ed608c0a55
yuling <gb2013429@gmail.com>
2024-01-19 16:56:40 +0800

    update-router-5

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,4 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    // 啟動 server 時預設開啟的頁面
-    open: '/pages/index.html',
-  },
 });
